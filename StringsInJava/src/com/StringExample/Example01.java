dfe05e4b259cc6feb65e726ea0d5b4d67ded0347
AvadhutJoshi012 <avadhutjoshi012@gmail.com>
2025-12-10 23:28:27 +0530

    Added String Example

---
+++ StringsInJava/src/com/StringExample/Example01.java
@@ -0,0 +1,17 @@
+package com.StringExample;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+
+		String name = "Avadhut";
+		System.out.println(name.length());
+		System.out.println(name.charAt(1));
+
+		String s = "HelloAvadhut";
+		System.out.println(s.substring(0, 5));
+		System.out.println(s.substring(5));
+
+	}
+
+}
